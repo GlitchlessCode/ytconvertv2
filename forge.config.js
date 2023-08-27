f85f52a6f8852e861b1a9d2409b263422a6b97e5
TurtleY0da <timothy.veugelers@gmail.com>
2023-08-27 15:01:09 -0600

    Config Adjustments
    + Added DMG maker

--- forge.config.js
+++ forge.config.js
@@ -5,25 +5,16 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      name: "@electron-forge/maker-dmg",
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
   ],
